65823164086d8d7164dbeef5d996e27a8196073a
Parth Choudhary <parth.dudhwal@gmail.com>
2023-10-12 09:43:55 +0530

    Completed Project

--- index.js
+++ index.js
@@ -19,78 +19,6 @@ close_btn.addEventListener("click", () => {
     renderCards();
 })
 
-// createArea.addEventListener("submit", (e) => {
-//     e.preventDefault();
-
-//     const title = titleInput.value;
-//     const content = contentInput.value;
-
-//     if (title.trim() !== "" && content.trim() !== "") {
-//         const note = { title, content };
-//         notes.push(note);
-
-//         titleInput.value = "";
-//         contentInput.value = "";
-
-//         renderCards();
-//     }
-// })
-
-// function renderCards() {
-//     notesList.innerHTML = "";
-
-//     if (notes.length === 0) {
-//         notes = [...emptyNotes];
-//     }
-
-//     notes.forEach((item, index) => {
-//         const note = document.createElement("div");
-//         note.classList.add("note");
-
-//         const noteTitle = document.createElement("h1");
-//         noteTitle.textContent = item.title;
-
-//         const noteContent = document.createElement("p");
-//         noteContent.textContent = item.content;
-
-//         const editButton = document.createElement("i");
-//         editButton.classList.add("fa-solid");
-//         editButton.classList.add("fa-pen-to-square");
-
-//         const deleteButton = document.createElement("i");
-//         deleteButton.classList.add("fa-solid");
-//         deleteButton.classList.add("fa-trash");
-
-//         deleteButton.addEventListener("click", () => {
-//             notes.splice(index, 1);
-
-//             renderCards();
-//         });
-
-//         editButton.addEventListener("click", () => {
-//             const editForm = createEditForm(index);
-//             note.replaceWith(editForm);
-//         });
-
-//         note.addEventListener("mouseenter", () => {
-//             editButton.style.display = "block";
-//             deleteButton.style.display = "block";
-//         });
-
-//         note.addEventListener("mouseleave", () => {
-//             editButton.style.display = "none";
-//             deleteButton.style.display = "none";
-//         });
-
-//         note.appendChild(noteTitle);
-//         note.appendChild(noteContent);
-//         note.appendChild(deleteButton);
-//         note.appendChild(editButton);
-
-//         notesList.appendChild(note);
-//     })
-// }
-
 let currentEditIndex = null; // Initialize a variable to track the index of the note being edited
 
 function createEditForm(index) {
@@ -134,19 +62,6 @@ createArea.addEventListener("submit", (e) => {
 
 const search = document.getElementById("search");
 
-// search.addEventListener("input", (e) => {
-//     const searchInput = search.value;
-
-//     console.log("changes");
-
-//     if(searchInput.trim() != ""){
-//         renderCards();
-//     }
-//     else{
-
-//     }
-// })
-
 search.addEventListener("input", () => {
     // Clear any existing search delay (if any)
     // clearTimeout(searchTimeout);
@@ -164,7 +79,8 @@ function renderCards(searchQuery = "") {
     notesList.innerHTML = "";
 
     if (notes.length === 0) {
-        notes = [...emptyNotes];
+        // notes = [...emptyNotes];
+        notes = JSON.parse(JSON.stringify([...emptyNotes]));
     }
 
     notes.forEach((item, index) => {
